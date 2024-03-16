58d7f144e03e3839e3591d3831fe18b95c90bf5e
joe95-ux <ogorktabi@gmail.com>
2024-03-16 16:40:05 +0200

    first cloudflare workers

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,30 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import {Ai} from "@cloudflare/ai"
+import { Hono } from 'hono'
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+export type Env = {
+  AI: any
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono<{ Bindings: Env}>()
+app.get("/", async c =>{
+  const ai = new Ai(c.env.AI)
+
+  const content = c.req.query("query") || 'What is the origin of the phrase Hello, World'
+
+  const messages = [
+    { role: 'system', content: 'You are a friendly assistant' },
+    { role: 'user', content }
+  ];
+
+  const inputs = { messages }
+
+  const res = await ai.run("@cf/mistral/mistral-7b-instruct-v0.1", inputs)
+
+  return c.json(res)
+
+})
+
+export default app
